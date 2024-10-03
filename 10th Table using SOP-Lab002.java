a2896292ffb07efa33742155a72c02745c50f740
vijaya608 <156274207+vijaya608@users.noreply.github.com>
2024-10-03 07:29:18 +0530

    Create 10th Table using SOP-Lab002.java
    Using only SOP

---
+++ 10th Table using SOP-Lab002.java
@@ -0,0 +1,31 @@
+public class Lab002{
+
+    public static void main(String[] args){
+        int a=10;
+        System.out.println("Output:\n"+ "Hello World!");
+        System.out.println("10*1=" + a*1);
+        System.out.println("10*1=" + a*2);
+        System.out.println("10*1=" + a*3);
+        System.out.println("10*1=" + a*4);
+        System.out.println("10*1=" + a*5);
+        System.out.println("10*1=" + a*6);
+        System.out.println("10*1=" + a*7);
+        System.out.println("10*1=" + a*8);
+        System.out.println("10*1=" + a*9);
+        System.out.println("10*1=" + a*10);
+
+    }
+}
+
+Output:
+
+10*1=10
+10*1=20
+10*1=30
+10*1=40
+10*1=50
+10*1=60
+10*1=70
+10*1=80
+10*1=90
+10*1=100
