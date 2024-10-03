0c082056a4ed292d79f11a2813b1d299a7aedd0e
vijaya608 <156274207+vijaya608@users.noreply.github.com>
2024-10-03 07:20:35 +0530

    Create Lab001.java
    My java Program

---
+++ Lab001.java
@@ -0,0 +1,20 @@
+public class Lab001{
+    public static void main(String[] args){
+        System.out.println("Output:\n"+ "Hello World!");
+
+    }
+}
+
+Output:
+Hello World!
+
+/*
+  public is access modifier
+  class is java keyword
+  Lab001 is filename/class name
+
+  Main method: add a main function to a Java application by using the keyword public static void main. It accepts parameters for command-line processing but cannot return values.
+public static void main(String[] args){
+  System is class
+    println is display the output message in the nextline
+}
