8a916ea737590428e05ffea78dbe9c87747a4c9a
vijaya608 <156274207+vijaya608@users.noreply.github.com>
2024-10-04 06:29:54 +0530

    Calculator Lab003.java

---
+++ Calculator Lab003.java
@@ -0,0 +1,23 @@
+public class Lab003{
+  public static void main(String[] args){
+    
+    System.out.print("Addition: ");
+    System.out.println(10+2);
+    System.out.print("Subtraction: ");
+    System.out.println(10-2);
+    System.out.print("Multiplication: ");
+    System.out.println(10*2);
+    System.out.print("Division: ");
+    System.out.println(10/2);
+    System.out.print("Modulus: ");
+    System.out.println(10%2);
+
+  }
+}
+
+Output:
+Addition: 12
+Subtraction: 8
+Multiplication: 20
+Division: 5
+Modulus: 0
